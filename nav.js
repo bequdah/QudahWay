5ad991ca7d94312f25877e0dc98c35673f4377cc
bequdah <bequdah@email.com>
2026-03-08 08:05:53 +0300

    Refine Mantra animation and add synced peeping cat effect

--- nav.js
+++ nav.js
@@ -220,9 +220,11 @@ document.addEventListener("DOMContentLoaded", function () {
         menuHTML += `
             <div class="nav-header" style="font-size: 1.1rem; color: #facc15;">Data Science</div>
             <a href="intro-to-ds.html" class="nav-link"> 01. Intro to Data Science</a>
-            <a href="Machine_and_Deap Learning.html" class="nav-link"> 02. Machine & Deep Learning</a>
-            <a href="Data_Preprocessing.html" class="nav-link"> 03. Data Preprocessing</a>
-            <a href="Fairness.html" class="nav-link"> 04. Fairness in AI</a>
+            <a href="Data_Preprocessing.html" class="nav-link"> 02. Data Preprocessing</a>
+            <a href="EDA.html" class="nav-link"> 03. Exploratory Data Analysis (EDA)</a>
+            <a href="Feature_Engineering.html" class="nav-link"> 04. Feature Engineering</a>
+            <a href="Machine_and_Deap Learning.html" class="nav-link"> 05. Machine & Deep Learning</a>
+            <a href="Fairness.html" class="nav-link"> 06. Fairness in AI</a>
         `;
     }
 
